d2c10b68973856764cd839b727bcc399ff79831f
iakshaykr <akshaykr9531@gmail.com>
2020-01-07 11:13:12 +0530

    hello bug

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World To New WEB APP using GIT only  & Devithub.com !"
+    return "What's up Hello World To New WEB APP using GIT only & Devithub.com !"
