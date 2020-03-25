a3c47ee94349d75a1d989825193b432995f0ef81
AKSHAY KUMAR <akshaykr9531@gmail.com>
2020-03-25 23:27:13 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "What's up Hello World To New WEB APP using GIT only & Devithub.com !"
+    return "What's up Hello World To New WEB APP using GIT & Devithub.com using DevOps tools!"
